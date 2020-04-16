bc9c2430a29937c4c502ff0fa047b3a4667ad9f4
Frédérique Mittelstaedt <git@gfm.io>
2020-04-16 12:36:57 +0100

    fix typo

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,3 @@
 /** Returns the GID for this group, if it exists, or else undefined */
-declare function getGidByName(username: string): number | undefined;
+declare function getGidByName(group: string): number | undefined;
 export default getGidByName;
